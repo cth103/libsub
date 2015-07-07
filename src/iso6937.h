dc1bedc93491fc2553252322425f9a5f860f1e5c
Carl Hetherington <cth@carlh.net>
2015-07-07 19:37:27 +0100

    Fix previous.

--- src/iso6937.h
+++ src/iso6937.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2014 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2014-2015 Carl Hetherington <cth@carlh.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,6 +16,8 @@
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 */
+
+#include <iostream>
 
 namespace sub {
 
